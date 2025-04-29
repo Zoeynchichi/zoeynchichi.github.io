aad29536474f34ee421ceced94f8586acddaba34
Zoeynchichi <hajujeong11@gmail.com>
2025-04-29 18:39:26 -0500

    add cat icon

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,21 @@
+import { FaCat } from 'react-icons/fa6';
 import { useNavigate } from 'react-router-dom';
+
+// const CatIcon = styled(FaCat)`
+//     color: orange;
+//     position: absolute;
+//     left: 20px;
+//     /* font-size: 200px; */
+//     width: 100px;
+//     height: 100px;
+// `;
 
 export default function Home() {
   const navigate = useNavigate();
   return (
     <>
+      {/* <CatIcon /> */}
+      <FaCat size={200} style={{ color: 'gray' }} />
       <div>This is homepage</div>
       <button onClick={() => navigate('/projects')}>Click Me</button>
     </>
